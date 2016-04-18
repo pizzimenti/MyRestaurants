a15d4c8c14a02c89d432ad10fdab3e40f93838c2
Brad Pizzimenti <brad@gennakersystems.com>
2016-04-18 00:51:48 -0700

    WIP pass data between activities

--- app/src/main/java/com/gennakersystems/myrestaurants/MainActivity.java
+++ app/src/main/java/com/gennakersystems/myrestaurants/MainActivity.java
@@ -3,23 +3,30 @@ package com.gennakersystems.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurants;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurants = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurants.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
